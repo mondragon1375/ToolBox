44ee2cbda66ed6edbbe62ef094ebab0629b0c878
mondragon1375 <mondragon1375@gmail.com>
2019-09-13 13:50:07 -0500

    new commit

--- app/src/main/java/com/example/toolbox/MainActivity.java
+++ app/src/main/java/com/example/toolbox/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.toolbox;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+    private NotificationManagerCompat //Stopped here, continue on 11:00 from https://www.youtube.com/watch?v=tTbd1Mfi-Sk
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendOnChannel1(View v){
+
+    }
+
+    public void sendOnChannel2(View v){
+
     }
 }
