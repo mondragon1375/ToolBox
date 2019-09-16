50285b7a9012dc9d054f28c0e444d0c044147060
mondragon1375 <mondragon1375@gmail.com>
2019-09-16 13:48:42 -0500

    got notifications to work

--- app/src/main/java/com/example/toolbox/MainActivity.java
+++ app/src/main/java/com/example/toolbox/MainActivity.java
@@ -1,27 +1,66 @@
 package com.example.toolbox;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
+
+import static com.example.toolbox.Notification.CHANNEL_1_ID;
+import static com.example.toolbox.Notification.CHANNEL_2_ID;
+
+//----------------- Help Gotten -----------------
+//Notifications - https://www.youtube.com/watch?v=tTbd1Mfi-Sk
+//Shared Preferences(Maybe) - https://www.androidauthority.com/how-to-store-data-locally-in-android-app-717190/
+//
+//-----------------------------------------------
 
 public class MainActivity extends AppCompatActivity {
-    private NotificationManagerCompat //Stopped here, continue on 11:00 from https://www.youtube.com/watch?v=tTbd1Mfi-Sk
+    private NotificationManagerCompat notificationManager;
+    private EditText editTextTitle, editTextMessage;
 
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notificationManager = NotificationManagerCompat.from(this);
+
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextMessage = findViewById(R.id.edit_text_message);
     }
 
     public void sendOnChannel1(View v){
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        android.app.Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_looks_one_black_24dp)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManager.notify(1, notification);
 
     }
 
     public void sendOnChannel2(View v){
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
 
+        android.app.Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.ic_looks_two_black_24dp)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .build();
+
+        notificationManager.notify(2, notification);
     }
 }
